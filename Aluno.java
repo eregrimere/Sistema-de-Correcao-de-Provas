1ed4bd24f507b3189240426bdbf6642b3eea4daf
eregrimere <102391765+eregrimere@users.noreply.github.com>
2026-05-27 10:42:18 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,18 @@
+public class Aluno {
+
+    private final String nome;
+    private final String respostas;
+
+    public Aluno(String nome, String respostas) {
+        this.nome = nome;
+        this.respostas = respostas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRespostas() {
+        return respostas;
+    }
+}
