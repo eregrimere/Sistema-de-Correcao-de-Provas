0f799649bc570ad8a909f4c0004588b2d5638728
eregrimere <102391765+eregrimere@users.noreply.github.com>
2026-05-27 10:43:05 -0300

    Create Resultado.java

---
+++ Resultado.java
@@ -0,0 +1,18 @@
+public class Resultado {
+
+    private final String nome;
+    private final int nota;
+
+    public Resultado(String nome, int nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+}
